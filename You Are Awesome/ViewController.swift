7ccc48fd3c1165d1c5b8f793fdd1ea574e126f11
Jack Sexton <jack@jacks-macbook-pro-3.local>
2018-01-29 16:16:25 -0500

    2.15 Commit

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -7,51 +7,86 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController
 {
     @IBOutlet weak var messageLabel: UILabel!
-     var index = 0
+    @IBOutlet weak var awesomeImage: UIImageView!
     
+    var awesomePlayer = AVAudioPlayer()
+    var index = -1
+    var imageNumber = -1
+    let numberOfImages = 3
+    var soundNumber = -1
+    let numberOfSounds = 3
+
     //Below executes when you first load the view
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("The View Loaded!")
-        
     }
-    @IBAction func showMesagePressed(_ sender: UIButton)
+    func playSound(soundName: String)
     {
-        let messages = ["You are Awesome!!","You are Great!", "You are Incredible"]
-        messageLabel.text = messages[index]
-        index = index+1
-        if index == messages.count
+        if let sound = NSDataAsset(name: soundName)
         {
-            index = 0
+            //Check to see
+            do
+            {
+                try awesomePlayer = AVAudioPlayer(data: sound.data)
+                awesomePlayer.play()
+            }
+                //Check if sound.data is not a valid audio file
+            catch
+            {
+                print("Error: data in \(soundName) couldn't be played as a sound")
+
+            }
         }
-        
-        }
-    // code from earlier
-  /*  let pi = 3.14
-    let messages = "You are Awesome!!"
-    let message2 = "You are Great!"
-    let message3 =  "You are Incredible"
-        if messageLabel.text == message1
-        {
-            messageLabel.text = message2
-            print(pi)
-        }
-        else if messageLabel.text == message2
-        {
-            messageLabel.text = message3
-        }
-        
         else
         {
-            messageLabel.text = message1
-        }*/
-            
+            //if reading didn't work report error
+            print ("Error: file \(soundName) didn't load")
+
         }
+    }
+    //Press Show Message
+    
+    func nonRepeatingRandom(lastNumber: Int, maxValue: Int) -> Int {
+        var newIndex = -1
+        repeat
+        {
+            newIndex = Int(arc4random_uniform(UInt32(maxValue)))
+        }   while lastNumber == newIndex
+        return newIndex
+    }
+
+    @IBAction func showButtonPressed(_ sender: UIButton) {
+    
+        let messages = ["You are Awesome!",
+                       "You are Great!",
+                        "You are Incredible!",
+                        "You are a Genius",
+                        "You are Phenomenal!"]
+
+        //display message
+        index = nonRepeatingRandom(lastNumber: index, maxValue: messages.count)
+        messageLabel.text = messages[(index)]
+
+        //show image
+        awesomeImage.isHidden = false
+        imageNumber = nonRepeatingRandom(lastNumber: imageNumber, maxValue: numberOfImages)
+        awesomeImage.image = UIImage(named: "image\(imageNumber+1)")
+    
+
+
+        soundNumber = nonRepeatingRandom(lastNumber: soundNumber, maxValue: numberOfSounds)
+
+        let soundName = "sound\(soundNumber)"
+        playSound(soundName: soundName)
+    }
+}
 
 
